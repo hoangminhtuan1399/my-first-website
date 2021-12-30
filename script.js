e6de0844abddae6193b837c7a79cf5518c61693e
Glitch (glitch-hello-website) <none>
2021-12-30 20:27:35 +0700

    new index.html

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-/*
-This is your site JavaScript code - you can add interactivity and carry out processing
-- Initially the JS writes a message to the console, and moves a button you can add from the README
-*/
+
 
 // Print a message in the browser's dev tools console each time the page loads
 // Use your menus or right-click / control-click and choose "Inspect" > "Console"
